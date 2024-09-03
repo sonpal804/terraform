84dae011f542f21240aedd7d2d707f85acded56e
Ravi <ravi@vytec.com>
2024-09-03 11:05:14 +0530

    secrets

--- provider.tf
+++ provider.tf
@@ -12,4 +12,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = var.client_secret
+  tenant_id       = "10000000-0000-0000-0000-000000000000"
+  subscription_id = "20000000-0000-0000-0000-000000000000"
 }
